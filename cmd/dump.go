158287eee51786717d22ba848a5981956960afd2
mukeshmahato17 <mukeshmahato1089@gmail.com>
2025-05-18 22:31:56 +0545

    Change to export as YML.

--- cmd/dump.go
+++ cmd/dump.go
@@ -6,21 +6,28 @@ import (
 	"os"
 	"time"
 
+	"github.com/mukeshmahato17/miniflux-sync/api"
 	"github.com/mukeshmahato17/miniflux-sync/config"
 	"github.com/mukeshmahato17/miniflux-sync/log"
 	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
 	miniflux "miniflux.app/v2/client"
 )
 
 func dump(ctx context.Context, flags *config.DumpFlags, client *miniflux.Client) error {
 	log.Info(ctx, "exporting data from miniflux")
-	data, err := client.Export()
+	feeds, _, err := api.FetchData(ctx, client)
 	if err != nil {
-		return errors.Wrap(err, "getting export data")
+		return errors.Wrap(err, "fetching data")
+	}
+
+	remoteState, err := api.GenerateDiffState(feeds)
+	if err != nil {
+		return errors.Wrap(err, "generating remote state")
 	}
 
 	// e.g. "miniflux-sync-remote-20240718105851_opml.xml"
-	filename := fmt.Sprintf("./miniflux-sync-remote-%s_opml.xml", time.Now().Format("20060102150405"))
+	filename := fmt.Sprintf("./miniflux-sync-remote-%s.yml", time.Now().Format("20060102150405"))
 	if flags.Path != "" {
 		log.Info(ctx, `using export path from "--path"`, log.Metadata{
 			"path": flags.Path,
@@ -29,7 +36,13 @@ func dump(ctx context.Context, flags *config.DumpFlags, client *miniflux.Client)
 	}
 
 	log.Info(ctx, "writing export data to file")
-	if err := os.WriteFile(filename, data, 0o600); err != nil { //nolint:mnd
+
+	dat, err := yaml.Marshal(remoteState.FeedURLsByCategoryTitle)
+	if err != nil {
+		return errors.Wrap(err, "marshalling remote state to yaml")
+	}
+
+	if err := os.WriteFile(filename, dat, 0o600); err != nil { //nolint:mnd
 		return errors.Wrap(err, "writing export data to file")
 	}
 
